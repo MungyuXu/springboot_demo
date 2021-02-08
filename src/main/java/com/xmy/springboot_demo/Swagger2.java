b52205c67c12ee57d22ed826e75339387f5299a8
Administrator <353991880@qq.com>
2021-02-08 14:26:44 +0800

    swagger类

--- src/main/java/com/xmy/springboot_demo/Swagger2.java
+++ src/main/java/com/xmy/springboot_demo/Swagger2.java
@@ -2,6 +2,7 @@ package com.xmy.springboot_demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class Swagger2 {
+public class Swagger2 implements WebMvcConfigurer {
 
     /**
      * 创建API应用
